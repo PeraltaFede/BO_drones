6e7a009efe9c612b4890d078b3d8693f6ed5da78
Federico Peralta <dev.peralta.federico@gmail.com>
2021-05-13 12:48:19 +0200

    Pruebas lengthscale

--- bin/Utils/utils.py
+++ bin/Utils/utils.py
@@ -9,6 +9,7 @@ w_obstacles = False
 a = []
 c = []
 
+import os.path
 
 # maxz = 0
 # meanz = 0
@@ -67,6 +68,33 @@ def create_map(grid, resolution, obstacles_on=False, randomize_shekel=True, sens
             name = path[-1] + '/data/Databases/numpy_files/random_{}.npy'.format(file)
         else:
             name = path[-1] + '/data/Databases/numpy_files/noisy_random_{}.npy'.format(file)
+
+        if not os.path.isfile(name):
+            no_maxima = 6
+            xmin = 0
+            xmax = 10
+            ymin = 0
+            ymax = 10
+            a = []
+            c = []
+            for i in range(no_maxima):
+                a.append([np.random.rand() * 9, np.random.rand() * 9])
+                c.append(5 + np.random.uniform(0.0, 1.0))
+            a = np.array(a)
+            c = np.array(c).T
+            _x = np.arange(xmin, xmax, resolution * (xmax - xmin) / (grid.shape[1]))
+            _y = np.arange(xmin, xmax, resolution * (ymax - ymin) / (grid.shape[0]))
+            _x, _y = np.meshgrid(_x, _y)
+            _z = np.fromiter(map(shekel_arg0, zip(_x.flat, _y.flat, grid.flat)), dtype=np.float,
+                             count=_x.shape[0] * _x.shape[1]).reshape(_x.shape)
+
+            meanz = np.nanmean(_z)
+            stdz = np.nanstd(_z)
+            _z = (_z - meanz) / stdz
+
+            with open(f'E:/ETSI/Proyecto/data/Databases/numpy_files/noise_random_{file}.npy', 'wb') as g:
+                np.save(g, _z)
+
         with open(name, 'rb') as g:
             # with open('E:/ETSI/Proyecto/data/Databases/numpy_files/ground_truth_norm.npy', 'rb') as g:
             # _z = np.load(g)
--- bin/v2/Examples/d_distance.py
+++ bin/v2/Examples/d_distance.py
@@ -6,34 +6,35 @@ path.extend([path[0][:path[0].rindex("bin") - 1]])
 from bin.Agents.gym_agent import SimpleAgent
 from bin.Simulators.gym_environment import GymEnvironment
 
-seeds = np.linspace(978462, 87549927, 100)
+seeds = np.linspace(978462, 87549927, 50)
 # optimal lengthscale is probably 153
-ds = [0.38466, 0.40466, 0.42466, 0.44466, 0.48466]
-for d in ds:
-    i = 0
-    for seed in seeds:
-        i += 1
-        np.random.seed(np.round(seed).astype(int))
-        drones = [SimpleAgent("t", _id=0, limited_distance=False)]
-        sim = GymEnvironment(path[-1] + "/data/Map/Simple/map.yaml", agents=drones, id_file=99,
-                             acq="gaussian_ei", acq_mod="truncated", render2gui=False, saving=True,
-                             name_file="gausian_ei_coupled_{}_1A1S".format(d), d=d, initial_pos="random")
-        try:
-            for k in range(25):
-                next_poses = []
-                for agent in sim.agents:
-                    if agent.reached_pose():
-                        next_poses.append(sim.coordinator.generate_new_goal(pose=agent.pose,
-                                                                            other_poses=[agt.pose for agt in
-                                                                                         sim.agents
-                                                                                         if
-                                                                                         agt is not agent]))
-                    else:
-                        next_poses.append([])
-                print(k, sim.step(next_poses))
-        except Exception as e:
-            sim.f.write(f"pos: {sim.agents[0].pose} " + str(e))
-        finally:
-            if sim.saving:
-                sim.f.close()
-        print(i)
+ds = [0.38466, 0.40466, 0.42466, 0.44466, 0.46466]
+for file in [97, 99]:
+    for d in ds:
+        i = 0
+        for seed in seeds:
+            i += 1
+            np.random.seed(np.round(seed).astype(int))
+            drones = [SimpleAgent("t", _id=0, limited_distance=False)]
+            sim = GymEnvironment(path[-1] + "/data/Map/Simple/map.yaml", agents=drones, id_file=file,
+                                 acq="gaussian_ei", acq_mod="truncated", render2gui=False, saving=True,
+                                 name_file="gausian_ei_coupled_{}_1A1S".format(d), d=d, initial_pos="random")
+            try:
+                for k in range(25):
+                    next_poses = []
+                    for agent in sim.agents:
+                        if agent.reached_pose():
+                            next_poses.append(sim.coordinator.generate_new_goal(pose=agent.pose,
+                                                                                other_poses=[agt.pose for agt in
+                                                                                             sim.agents
+                                                                                             if
+                                                                                             agt is not agent]))
+                        else:
+                            next_poses.append([])
+                    print(k, sim.step(next_poses))
+            except Exception as e:
+                sim.f.write(f"pos: {sim.agents[0].pose} " + str(e))
+            finally:
+                if sim.saving:
+                    sim.f.close()
+            print(i)
